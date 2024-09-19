6c8dac6e54496d348e2d78826f69b722f7ed8cb2
Rico Hancock <richardl@hancock1.com>
2024-09-19 15:05:37 -0600

    feat: :heavy_plus_sign: Add date-fns
    Added date-fns libarary

--- src/app/example.tsx
+++ src/app/example.tsx
@@ -4,16 +4,20 @@ import { QUERY_KEYS } from "@/lib/react-query";
 import { sampleFetch } from "@/functions";
 import { useQuery } from "@tanstack/react-query";
 import { minDelay } from "@/lib/helpers";
+import { format } from "date-fns";
 
 export default function Example() {
-  const { data } = useQuery({
+  const { data, dataUpdatedAt, ...rest } = useQuery({
     queryKey: [QUERY_KEYS.SAMPLE],
     queryFn: () => minDelay(sampleFetch(), 3000),
   });
 
+  console.log(rest);
+
   return (
     <div>
       <pre>{JSON.stringify(data, null, 2)}</pre>
+      <p className="text-xs">Updated at: {format(dataUpdatedAt, "pp")}</p>
     </div>
   );
 }
